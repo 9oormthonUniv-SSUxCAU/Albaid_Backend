6417b6077524bbafdb7d3e659271b21b5dcd9437
leejongsuu <lik9819@naver.com>
2024-09-24 15:49:12 +0900

    chore: Contract 패키지명 변경

--- src/main/java/Albaid/backend/domain/contract/api/ContractController.java
+++ src/main/java/Albaid/backend/domain/contract/api/ContractController.java
@@ -2,6 +2,7 @@ package Albaid.backend.domain.api;
 
 import Albaid.backend.domain.application.OcrService;
 import Albaid.backend.domain.application.dto.ContractDTO;
+import Albaid.backend.global.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class ContractController {
     private final OcrService ocrService;
 
     @PostMapping(value = "/upload", consumes = {MULTIPART_FORM_DATA_VALUE})
-    public ContractDTO save(@RequestPart MultipartFile contractImage) throws Exception {
-        return ocrService.extractContractInfo(contractImage);
+    public Response<ContractDTO> save(@RequestPart MultipartFile contractImage) throws Exception {
+        return Response.success(ocrService.extractContractInfo(contractImage));
     }
 }
